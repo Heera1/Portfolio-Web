31e3ea2bab4211f9141c31be971398550fcfc4d0
Heera1 <caecus.regio@gmail.com>
2023-07-10 22:24:12 +0900

    fix: 배포 에러 수정(9)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../src/components/Layout";
+import Layout from "../components/Layout";
 import { useRouter } from "next/router";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
